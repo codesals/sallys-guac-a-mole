1849abe9b96f258f17c8106dd452e547aec355d2
Sally <78157471+codesals@users.noreply.github.com>
2021-03-01 17:14:35 +0300

    Add timer that stops game when ends Backlog card 4 of 4 Bonus not yet done

--- src/Components/Box.js
+++ src/Components/Box.js
@@ -1,4 +1,4 @@
-import { BoxWrapper, StyledButton } from "../styles";
+import { BoxWrapper } from "../styles";
 import Holes from "./Holes";
 import { useState, useEffect } from "react";
 
@@ -8,13 +8,36 @@ const Box = () => {
   // // };
 
   const [score, setScore] = useState(0);
+  const [timeLeft, setTimeLeft] = useState(120);
+  const [isGameOn, setIsGameOn] = useState(1);
+
+  let time = 120;
+  // let timer;
+
+  const countDown = () => {
+    if (time > 0) {
+      time -= 1;
+      setTimeLeft(time);
+    } else {
+      setIsGameOn(0);
+      // clearInterval(timer);
+    }
+  };
+
+  useEffect(() => {
+    const timer = setInterval(countDown, 1000);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
 
   return (
     <>
       {/* <StyledButton onClick={startGame}>Start!</StyledButton> */}
+      <h3>Time Left: {timeLeft}</h3>
       <h3> Score: {score} </h3>
       <BoxWrapper>
-        <Holes score={score} setScore={setScore} />
+        <Holes score={score} setScore={setScore} isGameOn={isGameOn} />
       </BoxWrapper>
       {/* <p>{index}</p> */}
     </>
--- src/Components/Holes.js
+++ src/Components/Holes.js
@@ -3,11 +3,15 @@ import avocado from "../images/avocado.png";
 import guac from "../images/guac.png";
 import { useState, useEffect } from "react";
 
-//image URLs
-//https://www.redbubble.com/i/sticker/Cute-Fruit-Evil-Avocado-by-LycheeTees/49586299.EJUG5
-//https://ih1.redbubble.net/image.1275464908.6299/st,small,507x507-pad,600x600,f8f8f8.jpg
+// image URLs
+// Avocado:
+// https://www.redbubble.com/i/sticker/Cute-Fruit-Evil-Avocado-by-LycheeTees/49586299.EJUG5
+// https://ih1.redbubble.net/image.1275464908.6299/st,small,507x507-pad,600x600,f8f8f8.jpg
+// Guac:
+// https://webstockreview.net/image/avocado-clipart-guacamole/240734.html
+// https://webstockreview.net/images/avocado-clipart-guacamole-18.jpg
 
-const Holes = ({ score, setScore }) => {
+const Holes = ({ score, setScore, isGameOn }) => {
   const holes = [1, 2, 3, 4, 5]; //hole_ids
 
   const [avocadoLocation, setAvocadoLocation] = useState(0);
@@ -21,20 +25,22 @@ const Holes = ({ score, setScore }) => {
   };
 
   useEffect(() => {
-    const interval1 = setInterval(getRandom, 1100);
-    const interval2 = setInterval(() => setImage(avocado), 1100);
-    return () => {
-      clearInterval(interval1);
-      clearInterval(interval2);
-    };
-  }, []);
+    if (isGameOn) {
+      const interval1 = setInterval(getRandom, 1100);
+      const interval2 = setInterval(() => setImage(avocado), 1100);
+      return () => {
+        clearInterval(interval1);
+        clearInterval(interval2);
+      };
+    }
+  }, [isGameOn]);
 
   return holes.map((holenumber) => {
-    if (avocadoLocation === holenumber) {
+    if (avocadoLocation === holenumber && isGameOn) {
       return (
         <>
-          <StyledHole onClick={makeGuac}>
-            <img src={image} alt="hole" />
+          <StyledHole>
+            <img src={image} alt="hole" onClick={makeGuac} />
           </StyledHole>
           {/* <p> {score}</p> */}
         </>
